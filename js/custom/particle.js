355e7b0f18b29a8275230a04a6d77acd6e220e26
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-01-09 16:38:32 +0700

    add particle template

---
+++ js/custom/particle.js
@@ -0,0 +1,182 @@
+/*
+|--------------------------------------------------------------------------
+	Dora - Coming Soon Template SCRIPT JS
+|--------------------------------------------------------------------------
+*/
+
+$(document).ready(function() {
+
+	"use strict";
+
+	/*
+	|--------------------------------------------------------------------------
+		Notify Me
+	|--------------------------------------------------------------------------
+	*/
+
+  function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+  }
+  $($("#email")).keypress(function(e) {
+    if(e.which == 13) {
+        $(".notify").trigger('click');
+    }
+  });
+  $(".notify").on('click', function(){
+		var notify = $(".notify");
+    if(notify.hasClass("active")){
+      var email = $("#email");
+      if(isEmail(email.val()) == 0){
+        $(".notify-area").effect("shake");
+      }else{
+        //Type here when email is correct
+				notify.html("<i class='ion-checkmark-round'></i>").css("background", "#7dc139");
+        setTimeout(function(){
+          notify.removeClass("active").text("Notify Me").css("background", "#E91E63");
+          $(".notify-area input").removeClass("active");
+          $("#email").val('');
+
+        }, 1500);
+      }
+    }else{
+      $(".notify-area input").addClass("active");
+      notify.addClass("active").html("<i class='ion-android-send'></i>");
+    }
+  });
+  $(".overlay").on('click', function(){
+    $(".notify-area input").removeClass("active");
+    $(".notify").removeClass("active").html($(".notify").attr('data-title'));
+  });
+
+	/*
+	|--------------------------------------------------------------------------
+		Particle Js
+	|--------------------------------------------------------------------------
+	*/
+
+  particlesJS('particles-js',
+
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+  );
+
+
+});
