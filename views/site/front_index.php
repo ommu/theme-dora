96fdcda5619b1d807d24b9d2556a8c7d1c5d5a2b
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-08-02 11:25:10 +0700

    commit

--- views/site/front_index.php
+++ views/site/front_index.php
@@ -9,43 +9,34 @@
  *
  */
 
+	Yii::import('webroot.themes.dora.components.*');
+	$configTheme = Yii::app()->controller->themeSetting;
+
 	$this->breadcrumbs=array(
 		'Main',
 	);
-
 	$this->pageTitle = Yii::t('phrase', 'Main');
-	Yii::import('webroot.themes.dora.components.*');
-
-	$condition = 0;
-	$model = OmmuThemes::model()->findByAttributes(array(
-		'default_theme'=>1,
-		'folder'=>'dora',
-	));
-	$configTheme = unserialize($model->config);
-	if($model != null && is_array($configTheme) && !empty($configTheme))
-		$condition = 1;
 ?>
 
 <?php 
 $layout = 'LayoutGalaxy';
-if($condition) {
-	if($configTheme['template']['option'] == 'galaxy')
-		$layout = 'LayoutGalaxy';
-	else if($configTheme['template']['option'] == 'island')
-		$layout = 'LayoutIsland';
-	else if($configTheme['template']['option'] == 'pattern')
-		$layout = 'LayoutPattern';
-	else if($configTheme['template']['option'] == 'classic')
-		$layout = 'LayoutClassic';
-	else if($configTheme['template']['option'] == 'image')
-		$layout = 'LayoutImage';
-	else if($configTheme['template']['option'] == 'colorfulimage')
-		$layout = 'LayoutColorfulimage';
-	else if($configTheme['template']['option'] == 'particle')
-		$layout = 'LayoutParticle';
-	else if($configTheme['template']['option'] == 'stars')
-		$layout = 'LayoutStars';
-}
+if($configTheme['template']['option'] == 'galaxy')
+	$layout = 'LayoutGalaxy';
+else if($configTheme['template']['option'] == 'island')
+	$layout = 'LayoutIsland';
+else if($configTheme['template']['option'] == 'pattern')
+	$layout = 'LayoutPattern';
+else if($configTheme['template']['option'] == 'classic')
+	$layout = 'LayoutClassic';
+else if($configTheme['template']['option'] == 'image')
+	$layout = 'LayoutImage';
+else if($configTheme['template']['option'] == 'colorfulimage')
+	$layout = 'LayoutColorfulimage';
+else if($configTheme['template']['option'] == 'particle')
+	$layout = 'LayoutParticle';
+else if($configTheme['template']['option'] == 'stars')
+	$layout = 'LayoutStars';
+
 $construction_text = unserialize($setting->construction_text);
 $this->widget($layout, array(
 	'construction_date' => date('j M, Y H:i:s', strtotime($setting->construction_date)),
@@ -54,14 +45,14 @@ $this->widget($layout, array(
 	'color' => $configTheme['template']['color'],
 )); //Layout Galaxy ?>
 
-<?php if(!$condition || in_array($configTheme['portfolio']['publish'], array('', 1))) {
+<?php if($configTheme['portfolio']['publish'] != 0) {
 	$this->widget('ContentPortfolio', array(
 		'title' => $configTheme['portfolio']['title'],
 		'article_id' => $configTheme['portfolio']['article'],
 	));
 } //Portfolio ?>
 
-<?php if(!$condition || in_array($configTheme['contact-us']['publish'], array('', 1))) {
+<?php if($configTheme['contact-us']['publish'] != 0) {
 	$this->widget('ContentContactUs', array(
 		'title' => $configTheme['contact-us']['title'],
 		'desc' => $configTheme['contact-us']['desc'],
